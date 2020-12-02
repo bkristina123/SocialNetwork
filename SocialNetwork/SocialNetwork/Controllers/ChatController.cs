743079d128c98934db4c277686820c11f6ad06fd
Kristina <bkristina@live.com>
2020-12-02 22:54:25 +0100

    added messenger ui mock

---
+++ SocialNetwork/SocialNetwork/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Overview()
+        {
+            return View();
+        }
+    }
+}
